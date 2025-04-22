70f42ba069a7fc9d5b8d5717e65028034244e2f8
MajorDown <romain.fouillaron@gmx.fr>
2025-04-22 23:58:27 +0200

    séparation de la logique des tabs dans un fichier

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,58 +1,9 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
-type AppTab = {
-  name: string;
-  title: string,
-  for: 'user' | 'admin' | 'member';
-}
+import AppTabs from '@/constants/Tabs';
 
 export default function MainLayout() {
-
-  const AppTabs: AppTab[] = [
-    {
-      name: 'Outils',
-      title: 'les outils de la guilde',
-      for: 'admin',
-    },
-    {
-      name: 'Membres',
-      title: 'les membres de la guilde',
-      for: 'admin',
-    },
-    {
-      name: 'Arbitrage',
-      title: 'Arbitrage des contestations',
-      for: 'admin',
-    },
-    {
-      name: 'Règlement',
-      title: 'règlement de la guilde',
-      for: 'user',
-    },
-    {
-      name: 'Options',
-      title: 'options et paramètres',
-      for: 'user',
-    },
-    {
-      name: 'Déclarer',
-      title: 'déclarer une nouvelle intervention',
-      for: 'member',
-    },
-    {
-      name: 'Historique',
-      title: 'historique de vos interventions',
-      for: 'member',
-    },
-    {
-      name: 'Soldes',
-      title: 'les membres de la guilde',
-      for: 'member',
-    },
-
-  ]
 
   return (
     <Tabs
